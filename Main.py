22a193c7a60ca96ad5b962bef96e041e5c4f22b7
Mathew-Bushell <mathewbushell@live.co.uk>
2023-04-17 18:05:59 +0100

    f

--- Main.py
+++ Main.py
@@ -20,25 +20,22 @@ class Graph:
 
     def __init__(self, adjacency_list):
         self.adjacency_list = adjacency_list
-
+    #fetches the neigbours of the current city
     def getNeighbors(self, v):
         return self.adjacency_list[v]
 
-    # heuristic function with equal values for all nodes
+    # heuristic finds the value of a node within the heuristic dictionary
     def h(self, n):
         H = cityHeuristic
         return H[n]
 
     def aStarAlgorithm(self, start_node, stop_node):
-        # open_list is a list of nodes which have been visited, but who's neighbors
-        # haven't all been inspected, starts off with the start node
-        # closed_list is a list of nodes which have been visited
-        # and who's neighbors have been inspected
+        # open_list is a list of nodes which have been visited, but who's neighbors haven't all been inspected, starts off with the start node
+        # closed_list is a list of nodes which have been visited and who's neighbors have been inspected
         open_list = set([start_node])
         closed_list = set([])
 
-        # g contains current distances from start_node to all other nodes
-        # the default value (if it's not found in the map) is +infinity
+        # g contains current distances from start_node to all other nodes the default value (if it's not found in the map) is +infinity
         g = {}
         g[start_node] = 0
 
@@ -46,52 +43,51 @@ class Graph:
         parents = {}
         parents[start_node] = start_node
         while len(open_list) > 0:
-            n = None
+            newNode = None
             # find a node with the lowest value of f() - evaluation functio
             for v in open_list:
-                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
-                    n = v;
-            if n == None:
+                if newNode == None or g[v] + self.h(v) < g[newNode] + self.h(newNode):
+                    newNode = v;
+            if newNode == None:
                 print('Path does not exist!')
                 return None
 
-            # if the current node is the stop_node
-            # then we begin reconstructin the path from it to the start_nod
-            if n == stop_node:
+            # if the current node is the stop_node then we begin reconstructing the path from it to the start_node
+
+            if newNode == stop_node:
                 reconst_path = []
 
-                while parents[n] != n:
-                    reconst_path.append(n)
-                    n = parents[n]
+                while parents[newNode] != newNode:
+                    reconst_path.append(newNode)
+                    newNode = parents[newNode]
                 reconst_path.append(start_node)
                 reconst_path.reverse()
                 print('Path found: {}'.format(reconst_path))
                 return reconst_path
 
             # for all neighbors of the current node do
-            for (m, weight) in self.getNeighbors(n):
-                # if the current node isn't in both open_list and closed_list
-                # add it to open_list and note n as it's parent
+            for (m, weight) in self.getNeighbors(newNode):
+                # if the current node isn't in both open_list and closed_list add it to open_list and note n as it's parent
                 if m not in open_list and m not in closed_list:
                     open_list.add(m)
-                    parents[m] = n
-                    g[m] = g[n] + weight
+                    parents[m] = newNode
+                    g[m] = g[newNode] + weight
 
                 # otherwise, check if it's quicker to first visit n, then m
                 # and if it is, update parent data and g data
                 # and if the node was in the closed_list, move it to open_list
                 else:
-                    if g[m] > g[n] + weight:
-                        g[m] = g[n] + weight
-                        parents[m] = n
+                    if g[m] > g[newNode] + weight:
+                        g[m] = g[newNode] + weight
+                        parents[m] = newNode
                         if m in closed_list:
                             closed_list.remove(m)
                             open_list.add(m)
 
             # remove n from the open_list, and add it to closed_list
             # because all of his neighbors were inspected
-            open_list.remove(n)
-            closed_list.add(n)
+            open_list.remove(newNode)
+            closed_list.add(newNode)
 
         print('Path does not exist!')
         return None
