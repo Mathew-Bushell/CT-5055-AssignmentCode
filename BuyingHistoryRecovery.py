848d6f3ada54009f55fd146eea2737d78e6a8b11
Mathew-Bushell <mathewbushell@live.co.uk>
2023-05-04 20:04:21 +0100

    progress

--- BuyingHistoryRecovery.py
+++ BuyingHistoryRecovery.py
@@ -1,8 +1,10 @@
 import pandas as pd
-
-
-
-
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+import sklearn
+from sklearn.preprocessing import MinMaxScaler
 
 def verifymissing():
     file = open("buying_history.csv", "r")
@@ -10,7 +12,7 @@ def verifymissing():
     lastYear = "2009"
     lastMonth = "12"
     for line in file:
-        print(line)
+
         if line == "Year; Month;ItemsBought\n":
 
             continue
@@ -36,12 +38,35 @@ def verifymissing():
                 print("Month Bad")
 
 def modelTrain():
+    # unpacks and formats the dataset, sorts it into date and items brought
     buyHistory = pd.read_csv("buying_history.csv")
+    date = []
+    trainingCount=[]
+    validationCount=[]
+    info = buyHistory['Year;Month;ItemsBought'].tolist()
+    for i in info:
+        row = i.split(";")
+        date.append(row[0]+"-"+row[1])
+        trainingCount.append(row[2])
+        validationCount.append(row[2])
+    # date = pd.to_datetime(date, format='%Y-%m')
+    trainData = {'Brought': trainingCount}
+    trainingFrame = pd.DataFrame(data=trainingCount)
+    # print(trainingFrame)
+    tf.random.set_seed(7)
+    trainingSet = trainingFrame.values
+    trainingSet = trainingSet.astype('float32')
+    # normalises the dataset
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    trainingSet = scaler.fit_transform(trainingSet)
 
-    buyHistory.head(2)
-    buyHistory.dtypes
-    buyHistory['Year; Month;ItemsBought'].min(), buyHistory['Year; Month;ItemsBought'].max(), buyHistory.symbol.nunique()
+    # spits the training set into training and validation
+    trainSize = int(len(trainingSet) * 0.7)
+    validSize = len(trainingSet) - trainSize
+    train, validation = trainingSet[0:trainSize, :], trainingSet[trainSize:len(trainingSet), :]
+    print("Training = "+str(len(train)), "Validation = " + str(len(validation)))
 
 
-# modelTrain()
-verifymissing()
+
+modelTrain()
+# verifymissing()
